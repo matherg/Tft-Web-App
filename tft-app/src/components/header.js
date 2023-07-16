e56178124b744515b49cfa6d5f6917acf6893806
Graham Mather <mather.g@northeastern.edu>
2023-07-16 14:00:26 -0400

    Setup works

---
+++ tft-app/src/components/header.js
@@ -0,0 +1,14 @@
+const headerStyle = {
+    backgroundColor: "purple",
+    color: "white",
+    width: "100%",
+    height: "50px"
+};
+
+const Header = () => (
+    <div className="Header" style={headerStyle}>
+        Big Beautiful Banner
+    </div>
+);
+
+export default Header;
